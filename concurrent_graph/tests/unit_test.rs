8f2db703c379f29fe925ca15cb7c3fb9084a0b75
dSamp41 <d.sampietro@studenti.unipi.it>
2024-09-15 19:19:32 +0200

    Update unit_test.rs

--- concurrent_graph/tests/unit_test.rs
+++ concurrent_graph/tests/unit_test.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod tests{
     use std::collections::HashSet;
-    use concurrent_digraph::prelude::ConcurrentDiGraph;
+    use concurrent_graph::{ConcurrentDiGraph, ConcurrentUnGraph};
+    use concurrent_graph::GraphTrait;
 
     #[test]
     fn creation_test(){
@@ -76,5 +77,43 @@ mod tests{
         let mut j = HashSet::new();
         j.insert(0);
         assert_eq!(g.neighbors_incoming(2), j);
+    }
+
+
+    #[test]
+    fn un_dir_closed_neigh(){
+        let g = ConcurrentDiGraph::new();
+        g.add_edge(0, 1);
+        g.add_edge(0, 2);
+        g.add_edge(0, 3);
+        g.add_edge(1, 4);
+        g.add_edge(0, 1);
+        g.add_edge(2, 5);
+        g.add_edge(2, 8);
+        g.add_edge(3, 6);
+        g.add_edge(3, 7);
+        g.add_edge(5, 8);
+        g.add_edge(9, 10);
+
+        let h = ConcurrentUnGraph::new();
+        h.add_edge(0, 1);
+        h.add_edge(0, 2);
+        h.add_edge(0, 3);
+        h.add_edge(1, 4);
+        h.add_edge(0, 1);
+        h.add_edge(2, 5);
+        h.add_edge(2, 8);
+        h.add_edge(3, 6);
+        h.add_edge(3, 7);
+        h.add_edge(5, 8);
+        h.add_edge(9, 10);
+
+
+        println!("di {:?}", g.get_closed_neighborhoods_undirected());
+        println!("un {:?}", h.get_closed_neighborhoods_undirected());
+
+        //assert_eq!(g.get_closed_neighborhoods_undirected(), h.get_closed_neighborhoods_undirected());
+
+
     }
 }
