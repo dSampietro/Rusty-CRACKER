3ba96835f8f7df6a37105f02dd9c27ac5bd7a429
dSamp41 <d.sampietro@studenti.unipi.it>
2024-09-24 11:30:57 +0200

    Create a.py

---
+++ NX/a.py
@@ -0,0 +1,23 @@
+import networkx as nx
+from scipy.io import mmread
+#import fast_matrix_market as fmm
+import time
+
+start = time.time()
+BASE_PATH = "../files"
+filename = f"{BASE_PATH}/syn/V2_syn_2M_10M.mtx"
+
+
+#read graph
+a = mmread(filename)
+graph = nx.Graph(a)
+
+num_cc = nx.number_connected_components(graph)
+
+#connected components
+cc = nx.connected_components(graph)
+
+for _ in range(0, num_cc):
+    next(cc)
+
+print(f"duration: {1000 * (time.time() - start)} ms")
